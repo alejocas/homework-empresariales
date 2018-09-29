ecf1c103b52402f67a920596ba735472b0e51793
Alejo Castaño <alejocas98@gmail.com>
2018-09-28 20:52:04 -0500

    Trying to adjust horizontal cards

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="card">
+        <img :src="thumbnail" alt="Miniatura del producto" style="width:100%">
+        <div class="container">
+            <h4><b>{{title}}</b></h4> 
+            <p>{{price}}</p> 
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name:"ItemCard",
+  data() {
+    return {};
+  },
+  props: {
+    title: String,
+    price: Number,
+    thumbnail: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  width: 10%;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
+.container {
+  padding: 2px 16px;
+}
+</style>
--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -4,14 +4,22 @@
             <input type="text" name="valueSearched" v-model="query">
             <button type="submit" @click="searchItem">Buscar</button>
         </form>
-        <div class="item" v-for="item in items" :key="item">
-            <!-- Aquí van las tarjetas con productos. -->
-        </div>
+        <!-- Aquí van las tarjetas con productos. -->
+        <ul>
+            <li v-for="item in items" :key="item">
+                <ItemCard 
+                :title="item.title" 
+                :price="item.price" 
+                :thumbnail="item.thumbnail">
+                </ItemCard>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
 import axios from "axios";
+import ItemCard from "./ItemCard.vue";
 
 export default {
   name: "Shop",
@@ -21,15 +29,16 @@ export default {
       items: []
     };
   },
-  props: {},
-  mounted() {},
+  components: {
+    ItemCard
+  },
   methods: {
     searchItem(e) {
       e.preventDefault();
       if (this.query != "") {
         axios({
           method: "GET",
-          url: `https://api.mercadolibre.com/sites/MCO/search?q=${this.query}`
+          url: `https://api.mercadolibre.com/sites/MCO/search?q=${this.query}&limit=5`
         }).then(
           result => {
             this.items = result.data.results;
@@ -47,5 +56,8 @@ export default {
 
 <style lang="scss">
 .item {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
 }
 </style>
